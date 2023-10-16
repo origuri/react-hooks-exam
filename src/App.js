94a4547ab6f35ad6f393b5287f3389c50e108449
origuri <dlcksrb7@naver.com>
2023-10-16 15:28:36 +0900

    2023.10.16 - useTitle 완성

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ const useTitle = (initialTitle) => {
     const htmlTitle = document.querySelector("title");
     htmlTitle.innerHTML = title;
   };
-  //
+  // title의 값이 변경될 때에만 useEffect 되라.
   useEffect(updateTitle, [title]);
   return setTitle;
 };
 
+// titleUpdater("home") === useTitle("home") === setTitle("home")
 function App() {
   const titleUpdater = useTitle("Loading...");
   setTimeout(() => {
