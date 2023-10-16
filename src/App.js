cfe08f66cbda6877600571bff894d6d0fdfcebe8
origuri <dlcksrb7@naver.com>
2023-10-16 14:49:19 +0900

    2023.10.16 - useTabs 완성

--- src/App.js
+++ src/App.js
@@ -20,18 +20,23 @@ const useTabs = (initialTab, allTabs) => {
   }
 
   return {
+    // 객체 안에 변수를 선언하고 함수를 할당하여 리턴한 것.
+    // 그럼 useTabs는 changeItem을 리턴함.
+    // 결국 useTabs === changeItem === setCurrnetIndex
     currentItem: allTabs[currentIndex],
     changeItem: setCurrentIndex,
   };
 };
 
 // {}는 리턴 객체을 바로 가져올 수 있게 함
+// useTabs === changeItem === setCurrnetIndex 이므로
+// changeItem(index) === setCurrentIndex(index)
 function App() {
   const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div>
       {content.map((item, index) => (
-        <button key={index} onClick={(index) => changeItem}>
+        <button key={index} onClick={() => changeItem(index)}>
           {item.tab}
         </button>
       ))}
